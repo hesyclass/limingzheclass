438a44f83b19200e5591dfbffa0ffebaee431db2
jyeric <jyeric1@126.com>
2018-11-14 12:22:50 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<windows.h> 
+using namespace std; 
+int main()
+{
+	cout<<"它是数学的好伙伴";Sleep(900);system("cls");
+	cout<<"也是编程的一大盛举";Sleep(900);system("cls");
+	cout<<"它能帮助你学习数学";Sleep(900);system("cls");
+	cout<<"也能引起你对编程的兴趣";Sleep(900);system("cls");
+	cout<<"没错，你猜对了";Sleep(900);system("cls");
+	cout<<"他就是：";Sleep(1500);system("cls");
+	cout<<"李明喆小课堂！！！";Sleep(900);system("cls");
+	cout<<"最新消息，最新消息";Sleep(500);system("cls");
+	cout<<"李明喆";Sleep(500);system("cls");
+	cout<<"小课堂";Sleep(500);system("cls");
+	cout<<"开课啦！！！";Sleep(1000);system("cls");
+	cout<<"李明喆";Sleep(500);system("cls");
+	cout<<"小课堂";Sleep(500);system("cls");
+	cout<<"开课啦！！！";Sleep(1000);system("cls");
+	for(int i=0;i<4;i++){cout<<"最新";Sleep(400);}
+	cout<<"九种功能";
+	Sleep(600);system("cls");
+	cout<<"一切精彩，尽在李明喆小课堂";Sleep(2000);system("cls");
+)
