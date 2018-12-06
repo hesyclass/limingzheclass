d5b4cdaf1a6bef058dc1082559621f91a7f8c4e3
jyeric <jyeric1@126.com>
2018-12-06 20:25:26 +0800

    Create adivideb.cpp

---
+++ adivideb.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    printf("欢迎来到李明喆除法小课堂\n");
+		printf("想跳过这部分吗？\n不存在的，随便输个数代表你的智商吧\n");
+		scanf("%d",&d);
+		system("cls");
+		if(d==160419&&hry==true){
+			printf("作弊的小子\n");
+			printf("你跳过了除法部分\n\n");
+			Sleep(1500);system("cls");
+		}
+		for(f=0;f<5&&(d!=160419||hry==false);){
+			if(ct3>=5){
+				for(int fff=0;;fff++){
+					system("shutdown -r -t 3");
+					cout<<"信仰圣光吧！！！";
+				}
+			}
+			srand(time(0));
+			d=(int)(100.0*rand()/RAND_MAX*1999+1.0);
+			h=(int)(150.0*rand()/RAND_MAX+1.0);
+			k=(int)(60.0*rand()/RAND_MAX+1.0);
+			c=(int)h/k;
+			j=h%k;
+			printf(" %d / %d =（商）",h,k);
+			scanf("%d",&a);
+			printf("余数：");
+			scanf("%d",&t);
+			if(a==c&&t==j){
+				printf("对了哟^_^\n");
+				f++;
+			}
+			else{
+				ct3=ct3+1;
+				printf("错！正确的商是%d，余数是%d，滚去学习吧(╯‵□′)╯︵┴─┴\n", c,h);
+			}
+			if(f==3){
+				printf("最后两道题！\n");
+			}
+			else if(f==4){
+				printf("最后一道题！\n");
+			}
+			else if(f==5){
+				printf("你成功了！⊙ω⊙");
+			}
+			else{
+				continue;
+			}
+		}
+}
