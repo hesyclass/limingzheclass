95a98cf1a71eced8e1d23ccc7b9d3e3a3c256c88
jyeric <jyeric1@126.com>
2018-12-06 20:30:00 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std; 
 int main()
 {
+	string lmz;
 	cout<<"它是数学的好伙伴";Sleep(900);system("cls");
 	cout<<"也是编程的一大盛举";Sleep(900);system("cls");
 	cout<<"它能帮助你学习数学";Sleep(900);system("cls");
@@ -21,4 +22,8 @@ int main()
 	cout<<"九种功能";
 	Sleep(600);system("cls");
 	cout<<"一切精彩，尽在李明喆小课堂";Sleep(2000);system("cls");
-)
+	cout<<"谨记，输完什么都要回车！！！\n本程序由李明喆编写，胡瑞岩、辣鸡邱晨蔚续写。\n在同一个小课堂内错了五题以上后有一个微不足道的惩罚，嘿嘿嘿。\n";
+	cout<<"请输入你的名字：";
+	cin>>lmz;
+	return 0;
+}
